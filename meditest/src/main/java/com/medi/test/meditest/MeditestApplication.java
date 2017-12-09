c1d6804d417e215ceb437febea68a4fe3f4206d5
robertbutacu <robert.butacu@yahoo.com>
2017-12-09 11:44:25 +0200

    added backend project scaffold

---
+++ meditest/src/main/java/com/medi/test/meditest/MeditestApplication.java
@@ -0,0 +1,12 @@
+package com.medi.test.meditest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditestApplication.class, args);
+	}
+}
