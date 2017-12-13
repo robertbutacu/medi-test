b84d321397e64b751f6d98d2f7d9be28ff81642b
imlikefire <robertungureanu551@yahoo.com>
2017-12-13 21:55:06 +0200

    Added getTest method.

--- FrontEnd/meditest/src/app/test/test.service.ts
+++ FrontEnd/meditest/src/app/test/test.service.ts
@@ -9,4 +9,20 @@ export class TestService {
 
   constructor(private http: HttpService) {
   }
+
+  public getTest(numberOfQuestions: number, difficulty: string) {
+    if (!this.isGenerated) {
+      this.http.get("/questions?numberOfQuestions=" + numberOfQuestions + "&difficulty=" + difficulty)
+        .subscribe(
+          (response) => response.map(question => {
+            return {
+              question: question.body
+            }
+          }).forEach(question => this.questionList.push(question))
+        )
+      ;
+    }
+    this.isGenerated = true;
+    return this.questionList;
+  }
 }
