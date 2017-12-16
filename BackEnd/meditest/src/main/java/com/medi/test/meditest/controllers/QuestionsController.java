9aca1abc7dd76997c797051d6471e0bfc3dd12fc
robertbutacu <robert.butacu@yahoo.com>
2017-12-16 23:29:27 +0200

    fillInQuestions()

--- BackEnd/meditest/src/main/java/com/medi/test/meditest/controllers/QuestionsController.java
+++ BackEnd/meditest/src/main/java/com/medi/test/meditest/controllers/QuestionsController.java
@@ -168,7 +168,95 @@ public class QuestionsController {
     }
 
     private void generateFillInQuestions(Domain domain) {
+        Question question1 = new Question();
+        question1.setBody("Java is");
+        question1.setDifficulty(Difficulty.Hard);
+        question1.setQuestionType(QuestionType.FillIn);
 
+        Answer ans = new Answer();
+        ans.setBody("A Programming Language");
+        ans.setIsCorrect(true);
+        ans.setQuestion(question1);
+
+        Answer ans1 = new Answer();
+        ans1.setBody("An island");
+        ans1.setIsCorrect(false);
+        ans1.setQuestion(question1);
+
+        Answer ans2 = new Answer();
+        ans2.setBody("an animal");
+        ans2.setIsCorrect(false);
+        ans2.setQuestion(question1);
+
+        List<Answer> answers1 = new ArrayList<>();
+        answers1.add(ans);
+        answers1.add(ans1);
+        answers1.add(ans2);
+        question1.setAnswers(answers1);
+
+        Question question2 = new Question();
+        question2.setBody("Scala is...");
+        question2.setDifficulty(Difficulty.Hard);
+        question2.setQuestionType(QuestionType.FillIn);
+
+        Answer ans3 = new Answer();
+        ans3.setBody("A Functional Programming Language");
+        ans3.setIsCorrect(true);
+        ans3.setQuestion(question2);
+
+        Answer ans4 = new Answer();
+        ans4.setBody("A tasty beverage");
+        ans4.setIsCorrect(false);
+        ans4.setQuestion(question2);
+
+        Answer ans5 = new Answer();
+        ans5.setBody("A framework");
+        ans5.setIsCorrect(false);
+        ans5.setQuestion(question2);
+
+        List<Answer> answers2 = new ArrayList<>();
+        answers2.add(ans3);
+        answers2.add(ans4);
+        answers2.add(ans5);
+        question2.setAnswers(answers2);
+
+
+        Question question3 = new Question();
+        question3.setBody("Cobol is...");
+        question3.setDifficulty(Difficulty.Hard);
+        question3.setQuestionType(QuestionType.FillIn);
+
+        Answer ans6 = new Answer();
+        ans6.setBody("An ancient Programming Language");
+        ans6.setIsCorrect(true);
+        ans6.setQuestion(question3);
+
+        Answer ans7 = new Answer();
+        ans7.setBody("A rat");
+        ans7.setIsCorrect(false);
+        ans7.setQuestion(question3);
+
+        Answer ans8 = new Answer();
+        ans8.setBody("a mistake");
+        ans8.setIsCorrect(false);
+        ans8.setQuestion(question3);
+
+        List<Answer> answers3 = new ArrayList<>();
+        answers3.add(ans6);
+        answers3.add(ans7);
+        answers3.add(ans8);
+        question3.setAnswers(answers3);
+
+
+        List<Question> questions = new ArrayList<>();
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+
+        domain.setQuestions(questions);
+        questions.forEach(q -> q.setDomain(domain));
+
+        domainService.save(domain);
     }
 
     private void generateTrueOrFalseQuestions(Domain domain){
