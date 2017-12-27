d24b67e4eb8526f92bb918ef10c28d0d5a9feb4c
robertbutacu <robert.butacu@yahoo.com>
2017-12-27 15:12:07 +0200

    renamed domains table - no idea when i renamed it to relatedDomains

--- BackEnd/meditest/src/main/java/com/medi/test/meditest/entities/Domain.java
+++ BackEnd/meditest/src/main/java/com/medi/test/meditest/entities/Domain.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity(name = "Domain")
-@Table(name = "relatedDomains")
+@Table(name = "domains")
 public class Domain {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
