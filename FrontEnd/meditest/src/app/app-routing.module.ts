b855491410facc001461936db6293d3b676e2359
ursedaniel <daniel.urse@info.uaic.ro>
2018-01-17 23:23:14 +0200

    -modificare design proiect -securizare pagini -legatura generare test + afisare de intrebari -legatura login + register cu BE

--- FrontEnd/meditest/src/app/app-routing.module.ts
+++ FrontEnd/meditest/src/app/app-routing.module.ts
@@ -2,18 +2,16 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from "./login/components/login/login.component";
 import {RegisterComponent} from "./login/components/register/register.component";
+import {TestGeneratorComponent} from "./test/components/test-generator/test-generator.component";
+import {AuthGuard} from "./shared/guards/auth.guard";
+import {QuestionListComponent} from "./test/components/question-list/question-list.component";
+
 const routes: Routes = [
-  {
-    path: 'test',
-    children: [
-      {
-        path: '',
-        loadChildren: './test/test.module#TestModule'
-      }
-    ]
-  },
+  {path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'generate', component: TestGeneratorComponent, canActivate: [AuthGuard]},
+  {path: 'newtest', component: QuestionListComponent, canActivate: [AuthGuard]},
   {
     path: '**',
     redirectTo: ''
@@ -24,5 +22,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
