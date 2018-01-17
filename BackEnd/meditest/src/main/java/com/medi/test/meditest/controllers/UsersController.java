c5ff0862e29b626f5404d82424254bc8656e5dd6
eduardclatinici <Eduard.Clatinici@4finance.com>
2018-01-17 19:43:02 +0200

    refactoring: moved all logic from controller to service

--- BackEnd/meditest/src/main/java/com/medi/test/meditest/controllers/UsersController.java
+++ BackEnd/meditest/src/main/java/com/medi/test/meditest/controllers/UsersController.java
@@ -2,10 +2,8 @@ package com.medi.test.meditest.controllers;
 
 import com.medi.test.meditest.Transformers.UserTransformer;
 import com.medi.test.meditest.dtos.UserDto;
-import com.medi.test.meditest.entities.User;
 import com.medi.test.meditest.services.implementation.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,22 +18,14 @@ public class UsersController {
 
     @PostMapping("/login")
     public ResponseEntity<Long> login(@RequestBody UserDto userDto){
-        User user = service.getByUsername(userDto.getUsername());
-        if(user == null)
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        else
-            if(service.passwordMatches(userDto.getPassword(),user.getPassword()))
-                return new ResponseEntity<>(user.getId(), HttpStatus.OK);
-        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return service.checkCredentials(userDto, service.getByUsername(userDto.getUsername()));
     }
 
     @PostMapping("/register")
     public ResponseEntity register (@RequestBody UserDto userDto){
-        if(service.getByUsername(userDto.getUsername()) != null)
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        User user = transformer.toModel(userDto);
-        service.save(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return service.registerUser(userDto);
     }
+
+
 
 }
--- BackEnd/meditest/src/main/java/com/medi/test/meditest/services/implementation/UserService.java
+++ BackEnd/meditest/src/main/java/com/medi/test/meditest/services/implementation/UserService.java
@@ -1,9 +1,12 @@
 package com.medi.test.meditest.services.implementation;
 
+import com.medi.test.meditest.Transformers.*;
+import com.medi.test.meditest.dtos.*;
 import com.medi.test.meditest.entities.User;
 import com.medi.test.meditest.repositories.IUserRepository;
 import com.medi.test.meditest.services.contracts.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,8 @@ public class UserService implements IUserService{
 
     @Autowired
     IUserRepository repository;
+    @Autowired
+    UserTransformer transformer;
 
     private BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
 
@@ -46,7 +51,27 @@ public class UserService implements IUserService{
         return bCryptPasswordEncoder.encode(password);
     }
 
-    public Boolean passwordMatches(String password, String encodedPassword){
+    public ResponseEntity registerUser(UserDto userDto){
+        if(getByUsername(userDto.getUsername()) != null)
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        User user = transformer.toModel(userDto);
+        save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity<Long> checkCredentials(UserDto userDto, User user){
+        if(user == null)
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return passwordsMatch(userDto,user);
+    }
+
+    private ResponseEntity<Long> passwordsMatch(UserDto userDto, User user){
+        if(passwordMatchesHash(userDto.getPassword(),user.getPassword()))
+            return new ResponseEntity<>(user.getId(), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    private Boolean passwordMatchesHash(String password, String encodedPassword){
         return bCryptPasswordEncoder.matches(password,encodedPassword);
     }
 
