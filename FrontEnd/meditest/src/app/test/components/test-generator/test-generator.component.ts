59071d3a5c11eb8d237de9a73b057b9a47eb4918
robertbutacu <robert.butacu@yahoo.com>
2018-01-28 19:24:33 +0200

    english text changed to romanian

--- FrontEnd/meditest/src/app/test/components/test-generator/test-generator.component.ts
+++ FrontEnd/meditest/src/app/test/components/test-generator/test-generator.component.ts
@@ -23,52 +23,52 @@ export class TestGeneratorComponent implements OnInit {
   ];
 
   options = [
-    {value: 'level-time-0', viewValue: 'Difficulty level and time'},
-    {value: 'level-questions-1', viewValue: 'Difficulty level and number of questions'},
-    {value: 'questions-time-2', viewValue: 'Number of questions and time'}
+    {value: 'level-time-0', viewValue: 'Dificultate si durata'},
+    {value: 'level-questions-1', viewValue: 'Dificultate si numar de intrebari'},
+    {value: 'questions-time-2', viewValue: 'Numar de intrebari si timp'}
   ];
 
   levels = [
-    {value: 'Easy', viewValue: 'Easy'},
-    {value: 'Medium', viewValue: 'Normal'},
-    {value: 'Hard', viewValue: 'Hard'}
+    {value: 'Easy', viewValue: 'Usor'},
+    {value: 'Medium', viewValue: 'Mediu'},
+    {value: 'Hard', viewValue: 'Greu'}
   ];
 
   times = [
-    {value: '5', viewValue: '5 Minutes'},
-    {value: '10', viewValue: '10 Minutes'},
-    {value: '15', viewValue: '15 Minutes'},
-    {value: '20', viewValue: '20 Minutes'},
-    {value: '25', viewValue: '25 Minutes'},
-    {value: '30', viewValue: '30 Minutes'},
-    {value: '35', viewValue: '35 Minutes'},
-    {value: '40', viewValue: '40 Minutes'},
-    {value: '45', viewValue: '45 Minutes'},
-    {value: '50', viewValue: '50 Minutes'},
-    {value: '55', viewValue: '55 Minutes'},
-    {value: '60', viewValue: '60 Minutes'}
+    {value: '5', viewValue: '5 Minute'},
+    {value: '10', viewValue: '10 Minute'},
+    {value: '15', viewValue: '15 Minute'},
+    {value: '20', viewValue: '20 Minute'},
+    {value: '25', viewValue: '25 Minute'},
+    {value: '30', viewValue: '30 Minute'},
+    {value: '35', viewValue: '35 Minute'},
+    {value: '40', viewValue: '40 Minute'},
+    {value: '45', viewValue: '45 Minute'},
+    {value: '50', viewValue: '50 Minute'},
+    {value: '55', viewValue: '55 Minute'},
+    {value: '60', viewValue: '60 Minute'}
   ];
 
   questions = [
-    {value: '6', viewValue: '6 questions'},
-    {value: '7', viewValue: '7 questions'},
-    {value: '8', viewValue: '8 questions'},
-    {value: '9', viewValue: '9 questions'},
-    {value: '10', viewValue: '10 questions'},
-    {value: '11', viewValue: '11 questions'},
-    {value: '12', viewValue: '12 questions'},
-    {value: '13', viewValue: '13 questions'},
-    {value: '14', viewValue: '14 questions'},
-    {value: '15', viewValue: '15 questions'},
-    {value: '16', viewValue: '16 questions'},
-    {value: '17', viewValue: '17 questions'},
-    {value: '18', viewValue: '18 questions'},
-    {value: '19', viewValue: '19 questions'},
-    {value: '20', viewValue: '20 questions'},
-    {value: '21', viewValue: '21 questions'},
-    {value: '22', viewValue: '22 questions'},
-    {value: '23', viewValue: '23 questions'},
-    {value: '24', viewValue: '24 questions'}
+    {value: '6', viewValue: '6 intrebari'},
+    {value: '7', viewValue: '7 intrebari'},
+    {value: '8', viewValue: '8 intrebari'},
+    {value: '9', viewValue: '9 intrebari'},
+    {value: '10', viewValue: '10 intrebari'},
+    {value: '11', viewValue: '11 intrebari'},
+    {value: '12', viewValue: '12 intrebari'},
+    {value: '13', viewValue: '13 intrebari'},
+    {value: '14', viewValue: '14 intrebari'},
+    {value: '15', viewValue: '15 intrebari'},
+    {value: '16', viewValue: '16 intrebari'},
+    {value: '17', viewValue: '17 intrebari'},
+    {value: '18', viewValue: '18 intrebari'},
+    {value: '19', viewValue: '19 intrebari'},
+    {value: '20', viewValue: '20 intrebari'},
+    {value: '21', viewValue: '21 intrebari'},
+    {value: '22', viewValue: '22 intrebari'},
+    {value: '23', viewValue: '23 intrebari'},
+    {value: '24', viewValue: '24 intrebari'}
   ];
 
   constructor(public testService: TestService,
